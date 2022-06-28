3f3f42dd78bb2b03c9bc441863f8487453141cc3
Ryo <21cm0108@jec.ac.jp>
2022-06-28 09:35:11 +0900

    Initial Commit

---
+++ Filter08/Filter08App.swift
@@ -0,0 +1,17 @@
+//
+//  Filter08App.swift
+//  Filter08
+//
+//  Created by Ryo on 2022/06/28.
+//
+
+import SwiftUI
+
+@main
+struct Filter08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
